8dcf80c3bdf8a0b6279a45818430a9c0c4e2e2cc
josealon50 <josel4404@gmail.com>
2021-12-23 11:17:03 -0800

    Added autoloader

---
+++ autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+function SynchronyAutoload($className){ 
+    $array_paths = array(
+        'libs',
+        'db'
+    );
+    foreach($array_paths as $path)
+    {
+        $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+        $filename = __DIR__ . DIRECTORY_SEPARATOR .$path . DIRECTORY_SEPARATOR . $className . ".php";
+
+        if(is_file($filename))
+        {
+            include_once $filename;
+            break;
+        }
+
+    }
+}
+
+
+spl_autoload_register('KlaviyoAutoload');
+
+
+
